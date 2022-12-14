4be93f212c9ec5ab394b4f3eb6ebef316fdb8e23
Mellymel <120588074+Snowflake-Trail@users.noreply.github.com>
2022-12-14 23:12:35 +0530

    Update streamlit_app.py
    added menu

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,2 +1,7 @@
 import streamlit
 streamlit.title('My family new healthy diner')
+
+streamlit.header('Breakfast menu')
+streamlit.text('Omega 3 & Blueberries Oatmeal')
+streamlit.text('Kale, Spinach & Rocket Smoothie')
+streamlit.text('Hard-Boiled Free-range Egg')
